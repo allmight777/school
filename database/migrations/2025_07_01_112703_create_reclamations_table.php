7c0fa6b4f7821e88747d909696dc04ad4b01a646
allmight777 <agoliganange15@gmail.com>
2025-07-28 15:03:40 +0100

    Update 2025_07_01_112703_create_reclamations_table.php

--- database/migrations/2025_07_01_112703_create_reclamations_table.php
+++ database/migrations/2025_07_01_112703_create_reclamations_table.php
@@ -10,25 +10,19 @@ return new class extends Migration
     {
         Schema::create('reclamations', function (Blueprint $table) {
             $table->id();
-            
-            // Relations obligatoires
+
+
             $table->foreignId('matiere_id')->constrained('matieres');
             $table->foreignId('periode_id')->constrained('periodes_academiques');
-            $table->foreignId('annee_academique_id')->constrained('annee_academique');
+            $table->foreignId('annee_academique_id')->constrained('annee_academique'); 
+            $table->foreignId('bulletin_id')->constrained('bulletins');
             $table->foreignId('eleve_id')->constrained('eleves');
-            $table->foreignId('note_id')->constrained('notes')->onDelete('cascade');
-            
-            // Relations optionnelles
-            $table->foreignId('bulletin_id')->nullable()->constrained('bulletins');
-            $table->foreignId('professeur_id')->nullable()->constrained('professeurs')->onDelete('set null');
-            
-            // Champs standards
+
             $table->string('type_evaluation');
             $table->string('type');
             $table->text('description');
             $table->string('statut')->default('nouvelle');
-   
-            
+
             $table->timestamps();
         });
     }
