705a9fa7a6f0787ef9990dd416aa667a05b726d7
allmight777 <agoliganange15@gmail.com>
2025-07-28 15:05:15 +0100

    Update 2025_07_09_150321_modify_bulletin_id_in_reclamations_table.php

--- database/migrations/2025_07_09_150321_modify_bulletin_id_in_reclamations_table.php
+++ database/migrations/2025_07_09_150321_modify_bulletin_id_in_reclamations_table.php
@@ -1,1 +1,34 @@
+<?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyBulletinIdInReclamationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reclamations', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('bulletin_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reclamations', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('bulletin_id')->nullable(false)->change();
+        });
+    }
+}
