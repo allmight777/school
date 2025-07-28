373e52b19d32754eca1834d784d3b89e1486208a
allmight777 <agoliganange15@gmail.com>
2025-07-28 15:04:16 +0100

    Update 2025_07_03_100933_add_note_id_to_reclamations_table.php

--- database/migrations/2025_07_03_100933_add_note_id_to_reclamations_table.php
+++ database/migrations/2025_07_03_100933_add_note_id_to_reclamations_table.php
@@ -1,2 +1,29 @@
 
 
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up()
+{
+    Schema::table('reclamations', function (Blueprint $table) {
+        $table->unsignedBigInteger('note_id')->nullable()->after('eleve_id');
+        $table->foreign('note_id')->references('id')->on('notes')->onDelete('cascade');
+    });
+}
+
+public function down()
+{
+    Schema::table('reclamations', function (Blueprint $table) {
+        $table->dropForeign(['note_id']);
+        $table->dropColumn('note_id');
+    });
+}
+};
